4dc6bf44d1608511586837772f96e19e6e752bb2
AkkaRin11 <artemcrepper@gmail.com>
2024-11-12 23:05:16 +0300

    add base spring app

---
+++ backend/NaimixCodeHackathon/src/test/java/com/example/NaimixCodeHackathon/NaimixCodeHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NaimixCodeHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaimixCodeHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
